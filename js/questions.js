b24be1440012e79437fe6e4ba55272f891c24232
Théo Alves Dantas <108310203+TheoAlves@users.noreply.github.com>
2023-06-26 22:39:03 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,119 @@
+// criando um array e passando o número, perguntas, opções e respostas
+let questions = [
+    {
+    numb: 1,
+    question: "Qual o nome do protagonista do anime “Naruto”?",
+    answer: "(a) Naruto",
+    options: [
+       "(a) Naruto",
+       "(b) Bachira",
+       "(c) Mihawk",
+       "(d) Dazai"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual é o plural de “qualquer cidadão”?",
+    answer:  "(d) Quaisquer cidadãos",
+    options: [
+       "(a) Qualqueres cidadons",
+       "(b) Qualqueri cidadãos",
+       "(c) Aqueles cidadões",
+	     "(d) Quaisquer cidadãos"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quantas letras tem o alfabeto?",
+    answer:   "(b) 26 letras",
+    options: [
+          "(a) 24 letras",
+          "(b) 26 letras",
+          "(c) 27 letras",
+	        "(d) 25 letras"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Que pais tem na bandera uma espécie de uma folha?",
+    answer:  "(c) Canadá",
+    options: [
+     "(a) Japão",
+     "(b) Coreia do Norte",
+     "(c) Canadá",
+	   "(d) Rússia"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual é o território chinês que, tem o português como língua oficial?",
+    answer: "(a) Macau",
+    options: [
+      "(a) Macau",
+      "(b) Sonl",
+      "(c) Udgenji",
+      "(d) Sinagay"
+    ]
+  },
+
+   {
+    numb: 6,
+    question: "Quem foi a primeira pessoa a viajar no Espaço?",
+    answer: "(b) Yuri Gagarin",
+    options: [
+      "(a)  Buzz Aldrin",
+      "(b) Yuri Gagarin",
+      "(c) A cadela Laika",
+      "(d) Neil Armstrong"
+    ]
+  },
+  
+   {
+    numb: 7,
+    question: "Onde se localiza Machu Picchu?",
+    answer: "(a)  Peru",
+    options: [
+      "(a)  Peru",
+      "(b) China",
+      "(c) Bolívia",
+      "(d)  Índia"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Quem inventou a lâmpada?",
+    answer: "(c) Thomas Edison",
+    options: [
+      "(a)  Graham Bell",
+      "(b) Steve Jobs",
+      "(c) Thomas Edison",
+      "(d) Péle"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Quantos ossos temos no nosso corpo?",
+    answer: "(b) 206",
+    options: [
+      "(a) 126",
+      "(b) 206",
+      "(c) 18",
+      "(d) 300"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Qual o livro mais vendido no mundo a seguir da Bíblia?",
+    answer: "(d) Dom Quixote",
+    options: [
+      "(a) Ela, a feiticeira",
+      "(b) O senhor dos anéis",
+      "(c) O pequeno principe",
+      "(d) Dom Quixote"
+    ]
+  },
+
+];
